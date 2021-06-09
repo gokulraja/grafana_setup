7335f70c7a8e6493c11a92649cec375e727dfd30
manofsteel0007 <66902514+manofsteel0007@users.noreply.github.com>
2021-06-09 13:17:06 +0530

    9june 1.17

--- client-setup.sh
+++ client-setup.sh
@@ -8,7 +8,11 @@ wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_e
 
 tar xvzf node_exporter-1.1.2.linux-amd64.tar.gz
 
-sudo wget URL --output /etc/systemd/system/node_exporter.service
+sudo wget https://raw.githubusercontent.com/manofsteel0007/grafana_setup/main/node_exporter.service -O /etc/systemd/system/node_exporter.service
+
+subnet=$(ip -o -f inet addr show | awk '/scope global/ {print $4}')
+
+sudo ufw allow from $subnet to any port 9100
 
 sudo systemctl daemon-reload
 sudo systemctl start node_exporter
