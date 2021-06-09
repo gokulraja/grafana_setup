9f60dd826a558f5e90dba09cef6ecef78309a1be
manofsteel0007 <66902514+manofsteel0007@users.noreply.github.com>
2021-06-09 13:55:40 +0530

    1.55

--- client-setup.sh
+++ client-setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-mkdir /opt/node_exporter
+sudo mkdir /opt/node_exporter
 
-cd /opt/node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz -O /opt/node_exporter
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz 
+sudo tar xvzf node_exporter-1.1.2.linux-amd64.tar.gz
 
-tar xvzf node_exporter-1.1.2.linux-amd64.tar.gz
-
-sudo wget https://raw.githubusercontent.com/manofsteel0007/grafana_setup/main/node_exporter.service -O /etc/systemd/system/node_exporter.service
+sudo wget https://raw.githubusercontent.com/manofsteel0007/grafana_setup/main/node_exporter.service -O /etc/systemd/system/
 
 subnet=$(ip -o -f inet addr show | awk '/scope global/ {print $4}')
 
